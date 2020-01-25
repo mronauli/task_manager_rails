87ba7063f058b3ecb5005b13c20ffc94cfd8fe6e
Maria Ronauli <mronauli@gmail.com>
2020-01-25 01:09:36 -0800

    add updates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+   @tasks = Task.all
   end
 
   def new
@@ -13,5 +13,28 @@ class TasksController < ApplicationController
       })
       task.save
       redirect_to '/tasks'
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to '/tasks'
   end
 end
